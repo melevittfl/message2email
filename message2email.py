d9599179c6adfc32ffe17dc3e7560319ed3b13c7
Mark Levitt <mlevitt@Marks-MBP.marklevitt.co.uk>
2015-04-19 14:11:04 +0100

    Add SMS handling

--- message2email.py
+++ message2email.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+import os
+from postmark import PMMail
 
 app = Flask(__name__)
 
@@ -6,6 +8,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/message', methods=['POST'])
+def message():
+    sms = request.form['Body']
+    email = PMMail(api_key= os.environ.get('POSTMARK_API_TOKEN'),
+                   subject="One Time Token",
+                   sender="otptest@marklevitt.co.uk",
+                   to="markotp@marklevitt.co.uk",
+                   text_body=sms)
+    email.send()
+    return """<?xml version="1.0" encoding="UTF-8"?><Response></Response>"""
+
 
 
 if __name__ == '__main__':
